3042c6ba373cd482650bdc976a79c42d35bd3b73
Joshua Elliott <joshuacelliott@gmail.com>
2013-04-01 11:21:27 -0600

    Registered everything with the admin site

---
+++ lex/admin.py
@@ -0,0 +1,9 @@
+from lex.models import *
+from django.contrib import admin
+
+admin.site.register(Lexeme)
+admin.site.register(Representation)
+admin.site.register(PartOfSpeech)
+admin.site.register(FeatureSet)
+admin.site.register(WordSense)
+
---
+++ term/admin.py
@@ -0,0 +1,9 @@
+from lex.models import *
+from django.contrib import admin
+
+admin.site.register(TermBase)
+admin.site.register(SubjectField)
+admin.site.register(Concept)
+admin.site.register(ConceptDefinition)
+admin.site.register(Term)
+
