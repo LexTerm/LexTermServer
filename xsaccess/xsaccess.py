b561f055babca1dca16f79aed3cf747926460f3a
Logan Kearsley <chronosurfer@gmail.com>
2013-08-19 17:29:19 -0600

    Added allowed headers to access control

--- xsaccess/xsaccess.py
+++ xsaccess/xsaccess.py
@@ -8,12 +8,18 @@ except:
     XS_SHARING_ALLOWED_ORIGINS = '*'
     XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
 
+def setHeaders(req, resp):
+	resp['Access-Control-Allow-Origin']  = XS_SHARING_ALLOWED_ORIGINS
+	if req.method === "OPTIONS":
+		resp['Access-Control-Allow-Methods'] = ",".join(XS_SHARING_ALLOWED_METHODS)
+		if 'HTTP_ACCESS_CONTROL_REQUEST_HEADERS' in req.META:
+			resp['Access-Control-Allow-Headers'] = req.META['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']
+				
 class XsAccessMiddleware(object):
     def process_request(self, request):
         if 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
             response = http.HttpResponse()
-            response['Access-Control-Allow-Origin']  = XS_SHARING_ALLOWED_ORIGINS
-            response['Access-Control-Allow-Methods'] = ",".join(XS_SHARING_ALLOWED_METHODS)
+            setHeaders(request, response)
             return response
         return None
 
@@ -21,6 +27,5 @@ class XsAccessMiddleware(object):
         # Avoid unnecessary work
         if response.has_header('Access-Control-Allow-Origin'):
             return response
-        response['Access-Control-Allow-Origin']  = XS_SHARING_ALLOWED_ORIGINS
-        response['Access-Control-Allow-Methods'] = ",".join(XS_SHARING_ALLOWED_METHODS)
+        setHeaders(request, response)
         return response
