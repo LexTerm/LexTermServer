2ab0a2fe2463a4d888ae9f1efe37243bf8223ce0
Logan Kearsley <chronosurfer@gmail.com>
2013-08-29 18:21:49 -0600

    Simplified Access Control.

--- xsaccess/xsaccess.py
+++ xsaccess/xsaccess.py
@@ -1,25 +1,13 @@
 from django import http
 
-try:
-    import settings
-    XS_SHARING_ALLOWED_ORIGINS = settings.XS_SHARING_ALLOWED_ORIGINS
-    XS_SHARING_ALLOWED_METHODS = settings.XS_SHARING_ALLOWED_METHODS
-except:
-    XS_SHARING_ALLOWED_ORIGINS = '*'
-    XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
-
-def setHeaders(req, resp):
-	resp['Access-Control-Allow-Origin']  = XS_SHARING_ALLOWED_ORIGINS
-	if req.method == "OPTIONS":
-		resp['Access-Control-Allow-Methods'] = ",".join(XS_SHARING_ALLOWED_METHODS)
-		if 'HTTP_ACCESS_CONTROL_REQUEST_HEADERS' in req.META:
-			resp['Access-Control-Allow-Headers'] = req.META['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']
-				
 class XsAccessMiddleware(object):
     def process_request(self, request):
         if 'HTTP_ACCESS_CONTROL_REQUEST_METHOD' in request.META:
             response = http.HttpResponse()
-            setHeaders(request, response)
+            response['Access-Control-Allow-Origin']  = "*"
+            response['Access-Control-Allow-Methods'] = "OPTIONS,POST,GET,PUT,DELETE,HEAD"
+            if request.method == "OPTIONS":
+                response['Access-Control-Allow-Headers'] = request.META['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']
             return response
         return None
 
@@ -27,5 +15,6 @@ class XsAccessMiddleware(object):
         # Avoid unnecessary work
         if response.has_header('Access-Control-Allow-Origin'):
             return response
-        setHeaders(request, response)
+        response['Access-Control-Allow-Origin']  = "*"
+        response['Access-Control-Allow-Methods'] = "OPTIONS,POST,GET,PUT,DELETE,HEAD"
         return response
