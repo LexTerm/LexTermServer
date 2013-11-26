5d57170061654b1683a8e8fe12bf7c3661fd5fa5
Jason Housley <housleyjk@gmail.com>
2013-11-26 22:17:42 +0900

    restoring re import

--- tbx/views.py
+++ tbx/views.py
@@ -8,6 +8,7 @@ from rest_framework.reverse import reverse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.serializers import Serializer
+import re 
 
 try:
     from lxml import etree
